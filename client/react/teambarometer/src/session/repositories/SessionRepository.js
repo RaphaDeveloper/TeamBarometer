0755b32b298abbcdb94e44814b244a9c08b70cee
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-02-17 23:22:08 -0300

    Corrects the css and imports the models on repository

--- client/react/teambarometer/src/session/repositories/SessionRepository.js
+++ client/react/teambarometer/src/session/repositories/SessionRepository.js
@@ -1,10 +1,14 @@
+import Question from '../models/Question';
+import SessionModel from '../models/SessionModel';
+
 export default class SessionRepository {
     getSession() {
         const questions = [
-            new Question('Confiança', true),
-            new Question('Feedback'),
-        ];
-
-        return new SessionModel([questions]);
+            new Question('Confiança', false, 2, 2, 4),
+            new Question('Feedback', true),
+            new Question('Autonomia', false),
+          ];
+  
+          return new SessionModel(questions);
     }
 }
