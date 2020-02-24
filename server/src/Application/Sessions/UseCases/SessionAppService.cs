f18d340b6d12ce58c315d98bde3d5893fe52eb18
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-02-24 12:27:49 -0300

    Adds application layer and starts the implementation of the creation session use case in this layer. Application layer will be responsible to intermediate the delivery layer and the domain, this is usefull to segregate the responsibilities and reduce the coupling between them, because the domain and the objects consumed by the delivery layer changes for different reasons

---
+++ server/src/Application/Sessions/UseCases/SessionAppService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Sessions.UseCases;
+using System;
+
+namespace Application.Sessions.UseCases
+{
+	public class SessionAppService
+	{
+		public SessionAppService(SessionService sessionService)
+		{
+			SessionService = sessionService;
+		}
+
+		public SessionService SessionService { get; }
+
+		public SessionModel CreateSession(Guid facilitatorId)
+		{
+			return new SessionModel();
+		}
+	}
+}
---
+++ server/test/Application.Test/SessionAppServiceShould.cs
@@ -0,0 +1,27 @@
+﻿using Application.Sessions;
+using Application.Sessions.UseCases;
+using Domain.Questions;
+using Domain.Sessions.Repositories;
+using Domain.Sessions.UseCases;
+using NUnit.Framework;
+using System;
+
+namespace Application.Test
+{
+	public class SessionAppServiceShould
+	{
+		[Test]
+		public void CreateSession() 
+		{
+			Guid facilitatorId = Guid.NewGuid();
+			InMemorySessionRepository sessionRepository = new InMemorySessionRepository();
+			InMemoryQuestionTemplateRepository questionRepository = new InMemoryQuestionTemplateRepository();
+			SessionService sessionService = new SessionService(sessionRepository, questionRepository);
+			SessionAppService sessionAppService = new SessionAppService(sessionService);
+
+			SessionModel session = sessionAppService.CreateSession(facilitatorId);
+
+			Assert.NotNull(session);
+		}
+	}
+}
