a261ff3c9981653ad7bc50f530db6f93b10ec3ba
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-02-26 19:39:24 -0300

    Implements the first scenario of create session feature

---
+++ server/test/API.Specs/Session/Features/CreateSession.feature
@@ -0,0 +1,10 @@
+﻿Feature: CreateSession
+	In order to create a session
+	As a team member
+	I want to create a session to run a team barometer cerimony
+
+@mytag
+Scenario: Create session successfully
+	Given I am creating a session
+	When I request the creation
+	Then Session should be created successfully
---
+++ server/test/API.Specs/Session/Steps/CreateSessionSteps.cs
@@ -0,0 +1,49 @@
+﻿using BoDi;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using TechTalk.SpecFlow;
+
+namespace API.Specs.Session.Steps
+{
+    [Binding]
+    public class CreateSessionSteps
+    {
+        private readonly Context context;
+        private readonly HttpClient httpClient;
+
+        public CreateSessionSteps(Context context)
+        {
+            this.context = context;
+            this.httpClient = new HttpClient();
+        }
+
+        [Given(@"I am creating a session")]
+        public void GivenIAmCreatingASession()
+        {
+            context.Endpoint = "/sessions";
+        }
+
+        [When(@"I request the creation")]
+        public async void WhenIRequestTheCreation()
+        {
+            string uri = "http://localhost/teambarometer";
+            
+            string endpoint = uri + context.Endpoint;
+
+            context.HttpResponse = await httpClient.GetAsync(endpoint);
+        }
+
+        [Then(@"Session should be created successfully")]
+        public void ThenSessionShouldBeCreatedSuccessfully()
+        {
+            Assert.That(context.HttpResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+        }
+    }
+
+    public class Context
+    {
+        public string Endpoint { get; set; }
+        public HttpResponseMessage HttpResponse { get; set; }
+    }
+}
