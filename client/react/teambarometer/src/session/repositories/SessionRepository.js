2b4e76b9e0af04f364c7e92fdafa6987205c7ed1
silva.raphael.dev <silva.raphael.dev@hotmail.com>
2020-03-04 23:21:58 -0300

    Creates an environment variable to save the api url

--- client/react/teambarometer/src/session/repositories/SessionRepository.js
+++ client/react/teambarometer/src/session/repositories/SessionRepository.js
@@ -2,7 +2,7 @@ import SessionModel from '../models/SessionModel';
 
 export default class SessionRepository {
     async createSession() {
-        const response = await fetch(`http://localhost:5554/api/sessions/createsession/user/91E4AFD5-A3AF-40C1-8C54-A5829063BBCA`, { method: 'POST' });
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/sessions/createsession/user/91E4AFD5-A3AF-40C1-8C54-A5829063BBCA`, { method: 'POST' });
 
         const session = await response.json();
 
@@ -10,7 +10,7 @@ export default class SessionRepository {
     }
 
     async enterToTheSession(sessionId) {
-        const response = await fetch(`http://localhost:5554/api/sessions/jointhesession/${sessionId}/user/14907CD3-531F-4807-A458-FD6668FFFD19`, { method: 'PUT' });
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/sessions/jointhesession/${sessionId}/user/14907CD3-531F-4807-A458-FD6668FFFD19`, { method: 'PUT' });
 
         const session = await response.json();
 
